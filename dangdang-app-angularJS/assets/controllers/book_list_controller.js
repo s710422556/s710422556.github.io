fb430b81283ff056a26a9222028e47886d5b5cb4
s710422556 <Sun123456>
2016-10-25 12:01:25 -0700

    11

--- dangdang-app-angularJS/assets/controllers/book_list_controller.js
+++
@@ -1,7 +0,0 @@
-app.controller('bookListController',['$scope','$routeParams','commonService',function($scope,$routeParams,c_s){
-  $scope.data = [];
-  c_s.getData($routeParams.id,function(res){
-    // console.dir(res);
-    $scope.data = res.data;
-  });
-}]);
